c75857f054a3bd57cf0780f582c205fe6f935370
AexanderMorgunov <morgunowaleks@yandex.ru>
2021-09-11 10:44:06 +0300

    feat: add solution of task dream team

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,7 +13,28 @@ import { NotImplementedError } from '../extensions/index.js';
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-export default function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
+export default function createDreamTeam(members) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  
+  if(!(Array.isArray(members)))return false;
+    if(members.length==0)return false;
+    let teamName = '';
+
+
+
+for(let i=0;i<members.length;i++){
+    if(!members[i]) continue;
+    for(let j=0;j<members[i].length;j++){
+        if(!members[i][j]) continue;
+        if((members[i][j]!=' ')&&typeof(members[i][j]==String)){
+                    if(members[i][j].length>1){
+                        continue;
+                    } 
+        teamName+=members[i][j];
+        break;
+        }
+    }
+}
+    return Array.from(teamName.toUpperCase()).sort().join('');
 }
